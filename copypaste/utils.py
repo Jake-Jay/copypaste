b82e247dabc05271d18c0f17515bd2737e1b4ae7
Jake Pencharz <jake.pencharz@bayer.com>
2021-12-25 16:16:21 +0100

    utils: create visualisation function

---
+++ copypaste/utils.py
@@ -0,0 +1,20 @@
+import torch
+import matplotlib.pyplot as plt
+
+
+# ------------------------------------------------------------------------------
+# Visualisation Utilities
+# ------------------------------------------------------------------------------
+def plot_pair(img: torch.Tensor, cp: torch.Tensor) -> plt.figure():
+    fig, axs = plt.subplots(1, 2)
+
+    axs[0].imshow(img.moveaxis(0,-1))
+    axs[0].set_title('Original')
+    axs[1].imshow(cp.moveaxis(0,-1))
+    axs[1].set_title('Copy Paste')
+
+    for ax in axs:
+        ax.set_xticks([])
+        ax.set_yticks([])
+
+    return fig
